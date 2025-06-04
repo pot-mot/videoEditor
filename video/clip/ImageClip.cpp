3fc0734feb450a57ddbfb67ad52581de3e9e45b4
Potmot <377029227@qq.com>
2025-06-04 23:47:33 +0800

    FIX: chinese path for image

--- video/clip/ImageClip.cpp
+++ video/clip/ImageClip.cpp
@@ -1,16 +1,22 @@
 #include <opencv2/opencv.hpp>
 
 #include "ImageClip.h"
+#include "QFile"
 
 cv::Mat ImageClip::toMat() const {
-    cv::Mat image = cv::imread(filePath.toStdString(), cv::IMREAD_UNCHANGED);
+    QFile file(filePath);
+    if (!file.open(QIODevice::ReadOnly)) return cv::Mat();
+
+    QByteArray byteArray = file.readAll();
+    std::vector data(byteArray.data(), byteArray.data() + byteArray.size());
+    cv::Mat image = cv::imdecode(cv::Mat(data), cv::IMREAD_UNCHANGED);
 
     if (image.empty()) {
         // 如果图像为空，返回空矩阵
         return cv::Mat();
     }
 
-    for (MatEffect *effect : externalEffect) {
+    for (MatEffect *effect: externalEffect) {
         image = effect->apply(image);
     }
 
